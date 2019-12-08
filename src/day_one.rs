52df8764c5605fe59634b0378a9abed7e4ae8643
Chuckles <chuckleberryfinn@gmail.com>
2019-12-08 17:50:47 +0000

    Day one, part two

--- src/day_one.rs
+++ src/day_one.rs
@@ -6,33 +6,61 @@ use math::round;
 
 #[cfg(test)]
 mod calculate_tests {
-    use super::calculate;
+    use super::{calculate_fuel, calculate_module_fuel};
 
     #[test]
-    fn test_12() {
+    fn test_fuel_mass_12() {
         assert_eq!(
-            2, calculate(12)
+            2, calculate_fuel(12)
         );
     }
 
     #[test]
-    fn test_14() {
+    fn test_fuel_mass_14() {
         assert_eq!(
-            2, calculate(14)
+            2, calculate_fuel(14)
         );
     }
 
     #[test]
-    fn test_1969() {
+    fn test_fuel_mass_1969() {
         assert_eq!(
-            654, calculate(1969)
+            654, calculate_fuel(1969)
         );
     }
 
     #[test]
-    fn test_100756() {
+    fn test_fuel_mass_100756() {
         assert_eq!(
-            33583, calculate(100756)
+            33583, calculate_fuel(100756)
+        );
+    }
+
+    #[test]
+    fn test_fuel_module_12() {
+        assert_eq!(
+            2, calculate_module_fuel(12)
+        );
+    }
+
+    #[test]
+    fn test_fuel_module_14() {
+        assert_eq!(
+            2, calculate_module_fuel(14)
+        );
+    }
+
+    #[test]
+    fn test_fuel_module_1969() {
+        assert_eq!(
+            966, calculate_module_fuel(1969)
+        );
+    }
+
+    #[test]
+    fn test_fuel_module_100756() {
+        assert_eq!(
+            50346, calculate_module_fuel(100756)
         );
     }
 }
@@ -53,9 +81,25 @@ fn read_masses(filename: &str) -> Result<Vec<i64>, Error> {
 }
 
 
-fn calculate(mass: i64) -> i64 {
+fn calculate_fuel(mass: i64) -> i64 {
     let _mass = mass as f64;
-    (round::floor(_mass / 3.0, 0) - 2.0) as i64
+    let fuel = (round::floor(_mass / 3.0, 0) - 2.0) as i64;
+
+    if fuel <= 0 {
+        return 0;
+    }
+
+    fuel
+}
+
+
+fn calculate_module_fuel(mass: i64) -> i64 {
+    if mass <= 0 {
+        return 0;
+    }
+
+    let fuel = calculate_fuel(mass);
+    fuel + calculate_module_fuel(fuel)
 }
 
 
@@ -65,7 +109,7 @@ pub fn calculate_fuel_required() -> Result<i64, Error> {
     let mut fuel = 0;
 
     for mass in masses {
-        fuel += calculate(mass);
+        fuel += calculate_module_fuel(mass);
     }
     Ok(fuel)
 }
